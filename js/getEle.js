b9ea7a8247cb55f20ebf40c837ab45a020a85001
Carlos Alcalá <alcalacarcis@gmail.com>
2021-06-04 08:33:28 -0500

    Lista enlazada a la BD

---
+++ js/getEle.js
@@ -0,0 +1,28 @@
+const productContainer = document.getElementById("product-list");
+const getProductos = () => fs.collection("products").get();
+window.addEventListener("DOMContentLoaded", async (e) => {
+  const querySnapshot = await getProductos();
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+    const producto = doc.data();
+    productContainer.innerHTML += `<div class="col-md-4">
+    <section class="panel">
+        <div class="pro-img-box">
+            <img src="${producto.imagepath}" alt="" />
+            <a href="#" class="adtocart">
+                <i class="fa fa-shopping-cart">🛒</i>
+            </a>
+        </div>
+
+        <div class="panel-body text-center">
+            <h4>
+                <a href="./DetallePdto.html" class="pro-title">
+                    ${producto.nombre}
+                </a>
+            </h4>
+            <p class="price">S/.${producto.precio}</p>
+        </div>
+    </section>
+</div>`;
+  });
+});
